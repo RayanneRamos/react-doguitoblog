23e46d74106579c32e54b3a1a129db8b4505c437
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-14 10:23:47 -0300

    Chamando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-  <>
-    <Home />
-    <Sobre />
-  </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<Pagina404 />} />
+      </Routes>
+    </Router>
   )
 }
 

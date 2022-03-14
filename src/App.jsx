022605e4fd8812d4642ec5bea4c0f79bc22c0453
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-14 10:38:10 -0300

    Modificações

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Pagina404 from './paginas/Pagina404';
-import Cabecalho from './components/cabecalho';
+import Cabecalho from './components/Cabecalho';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {

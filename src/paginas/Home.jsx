19405778db842efc8ae0ea123abaf14b36e073ac
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-14 15:15:32 -0300

    Chamando o componente ListaCategorias na página Home

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import ListaCategorias from '../components/ListaCategorias'
 import ListaPost from '../components/ListaPost'
 
 const Home = () => {
-
   return (
     <main>
       <div className="container">
         <h2 className="titulo-pagina">Pet notícias</h2>
       </div>
+      <ListaCategorias />
       <ListaPost url={'/posts'} />
     </main>
   )

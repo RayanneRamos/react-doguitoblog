72ebf2697663556ad9fc25ac7a484b1adaecc861
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-14 16:06:52 -0300

    Chamando as subcategorias na página Categoria

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams} from 'react-router-dom';
+import { busca } from '../api/api';
 import '../assets/css/blog.css';
 import ListaCategorias from '../components/ListaCategorias';
 import ListaPost from '../components/ListaPost';
 
 const Categoria = () => {
   const { id } = useParams();
+  const [ subcategorias, setSubCategorias ] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubCategorias(categoria.subcategorias);
+    });
+  }, [id]);
 
   return (
     <>

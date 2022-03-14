6e2d09e7dd16362e584991a7e91e2831402056fe
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-03-14 10:34:22 -0300

    Chamando o menu nas páginas

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './components/cabecalho';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
